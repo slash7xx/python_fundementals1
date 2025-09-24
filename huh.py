a9c7354ea0fb1ce4dce2abd6454d7eaa504e4d5e
slash7xx <li20291684@student.nhvweb.net>
2025-09-24 16:19:04 +0000

    CSAEA

---
+++ huh.py
@@ -0,0 +1,34 @@
+#strings
+print("hello world")
+print(" ")
+print("Name: \tBilly \nAge: \t14 \nGrade: \t9th")
+
+#Integers
+print("41")
+print("2+4")
+print("3*5")
+
+#Floats
+print("3.14")
+print("0.1 + 0.2")
+print("6 / 2")
+
+#Booleans
+print("True")
+print("False")
+print("3 == 4")
+print("3 != 4")
+print("Hello" == "Hello")
+
+
+
+print(type("To see this message, wrap me in a print statement"))
+print(type(41))
+print(type(3.14))
+print(type(True))
+print(type(False))
+print(type(None))
+print(type([]))
+print(type({}))
+print(type(()))
+print(type(range(6)))
