65d3451289543720538c29e686a9c45cb3cf68fa
slash7xx <li20291684@student.nhvweb.net>
2025-09-26 18:03:15 +0000

    python_fundementals_math

---
+++ python_fundementals_math.py
@@ -0,0 +1,38 @@
+add = 7+2
+print("Sum:", add)
+
+subtract = 7-2
+print("Difference:", subtract)
+
+multiply = 7*2
+print("Product:", multiply)
+
+float_divide = 7/2
+print("Float Division:", float_divide)
+
+integer_divide = 7//2
+print("Integer Division:", integer_divide)
+
+modulus = 7%2
+print("Modulus:", modulus)
+
+exponent = 7**2
+print("Exponent:", exponent)
+
+print(2 + 2 == 10 - 6)
+
+result1 = (2 + 3) * 4
+print("Result 1:", result1)
+
+result2 = 2**3 * 4
+print("Result 2:", result2)
+
+result3 = 20 / 5 *2
+print("Result 3:", result3)
+
+result4 = 10 - 2 + 3
+print("Result 4:", result4)
+
+result 5 = 6 + 2 ** 3 * (4-1) # type: ignore
+print("Result 5:", result5) # type: ignore
+
---
+++ variables.py
@@ -0,0 +1,9 @@
+password = "00290835"
+email = "skibidi@gmail.com"
+print("Password: ", password)
+print("Email: ", email)
+
+x="4"
+y="hello"
+
+temp = y
