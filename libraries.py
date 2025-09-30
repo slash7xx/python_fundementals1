26764128d621a3719736968b2acb6f0d9590b456
slash7xx <li20291684@student.nhvweb.net>
2025-09-30 15:54:24 +0000

    math_libraries

---
+++ libraries.py
@@ -0,0 +1,8 @@
+import math
+print("\n--- Math Library ---\n")
+
+print("Square root:", math.sqrt(25))
+print("Round up to an integer:", math.ceil(4.2))
+print("Round down to an integer:", math.floor(4.8))
+print("Power:", math.pow(2, 5))
+print("Pi:", math.pi)
