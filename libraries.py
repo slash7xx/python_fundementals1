f7fdcfde43dc4391e49fdfd30803fdd1e8d5cc70
slash7xx <li20291684@student.nhvweb.net>
2025-09-30 16:27:20 +0000

    math_challenge

--- libraries.py
+++ libraries.py
@@ -1,8 +1,3 @@
-import math
-print("\n--- Math Library ---\n")
-
-print("Square root:", math.sqrt(25))
-print("Round up to an integer:", math.ceil(4.2))
-print("Round down to an integer:", math.floor(4.8))
-print("Power:", math.pow(2, 5))
-print("Pi:", math.pi)
+x = 8
+y = x - 247538 **9 / 13 + 9.278 - 7.991 * 3
+print(y)
