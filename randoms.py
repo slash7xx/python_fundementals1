5c2f313909b8b92c820b8a6777d57ffc7432e69b
slash7xx <li20291684@student.nhvweb.net>
2025-10-01 17:26:33 +0000

    random_numbers.py

---
+++ randoms.py
@@ -0,0 +1,15 @@
+import random
+import math
+
+
+print("Random integer: ", random.randint(1,7))
+print(random.random())
+print(random.choice(["taco", "burrito", "enchilada", "quesadilla"]))
+
+#Challenge 1
+radius = 7
+circle_area = radius ** 2 * math.pi
+print(circle_area)
+
+#Challenge 2
+print("Random integer between 1 and 6: ", random.random())
