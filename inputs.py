ef4fc334cc2972267c84583c9da036e8f123c8ac
slash7xx <li20291684@student.nhvweb.net>
2025-10-03 18:24:25 +0000

    inputs.py

---
+++ inputs.py
@@ -0,0 +1,35 @@
+print("\n--- User Input Demonstration ---")
+
+name = input("Enter your name: ")
+print("Hello", name)
+
+age = input("Enter your age: ")
+print(type(age))
+
+age = int(input("Enter your age: "))
+print(type(age))
+
+age_number = int(age)
+print("Next year, you will be:", age_number + 1)
+print(type(age_number))
+
+# Example with float input
+height = float(input("Enter your height in meters: "))
+print("You are", height, "meters tall.")
+
+
+#Challenge 1
+favorite_color = input("Favorite_color:")
+print("My favorite color is" ,favorite_color)
+
+#Challenge 2
+numbers = integer(input("Two numbers:"))
+print()
+
+#Challenge 3
+#did not figure it out
+
+#Challenge 4
+import random 
+sides = int(input("How many sides should the die have?"))
+print()
